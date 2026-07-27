bfe973be94cbc8c19b55aee0ebee866f2630c89f
providence-freight-solutions <shamus@providencefreight.com>
2026-07-26 20:53:52 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // Bump CACHE when you deploy a new console build.
 // =====================================================================
 
-const CACHE = 'cf-console-v3';
+const CACHE = 'cf-console-v4';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-maskable-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', (e) => {
